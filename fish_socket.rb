f3c2dee2ed26df2b22d9ab3ba62fe182485e2bb0
Apleon <sharapov.a.v.prog@gmail.com>
2022-03-20 14:25:03 +0300

    refactor: rename fishsocket.rb

--- fish_socket.rb
+++ fish_socket.rb
@@ -5,7 +5,11 @@ require './modules/listener'
 require './modules/security'
 require './modules/standart_messages'
 require './modules/response'
-Entry point class
+require './modules/callback_messages'
+require './modules/assets/inline_button'
+require './modules/promos'
+
+# Entry point class
 class FishSocket
   include Database
   def initialize
@@ -13,16 +17,16 @@ class FishSocket
     # Initialize BD
     Database.setup
     # Establishing webhook via @gem telegram/bot, using API-KEY
-    Telegram::Bot::Client.run(TelegramOrientedInfo::APIKEY) do |bot|
+    Telegram::Bot::Client.run(TelegramOrientedInfo::API_KEY) do |bot|
       # Start time variable, for exclude message what was sends before bot starts
-      startbottime = Time.now.toi
+      start_bot_time = Time.now.to_i
       # Active socket listener
       bot.listen do |message|
         # Processing the new income message    #if that message sent after bot run.
-        Listener.catchnewmessage(message,bot) if Listener::Security.messageisnew(startbottime,message)
+        Listener.catch_new_message(message,bot) if Listener::Security.message_is_new(start_bot_time,message)
       end
     end
   end
 end
-Bot start
+# Bot start
 FishSocket.new
--- library /mac-shake.rb
+++ library /mac-shake.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module TelegramOrientedInfo
-  API_KEY = '1363424466:AAHbIOp83WhmUWUcGdAkrp5uPuQK3qE3X2g'
+  API_KEY = 1363424466:AAHbIOp83WhmUWUcGdAkrp5uPuQK3qE3X2g
 end
